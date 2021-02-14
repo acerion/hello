73e44cc60633ec9076e39d84416821e0d1bf7561
acerion <acerion@wp.pl>
2021-02-14 22:33:05 +0100

    Adding copyright notice

--- src/haskell/cookies.hs
+++ src/haskell/cookies.hs
@@ -1,3 +1,25 @@
+{-
+Copyright (C) 2021 Kamil Ignacak acerion@wp.pl
+
+This file is part of "hello" web browser.
+
+"hello" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+"hello" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with "hello".  If not, see <https://www.gnu.org/licenses/>.
+
+This file is derived from dillo-3.0.5/src/cookies.c
+-}
+
+
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 
