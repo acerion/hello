c859be4bda94f2289af558e890850f15c5349d3c
acerion <acerion@wp.pl>
2021-02-17 22:54:15 +0100

    One more change needed to run new GIF tests

--- src/haskell/run_ghci.sh
+++ src/haskell/run_ghci.sh
@@ -1,1 +1,1 @@
-ghci -fobject-code  -XOverloadedStrings ./cookies.hs ./tests/TestsCookies.hs ./gif.hs
+ghci -fobject-code  -XOverloadedStrings ./cookies.hs ./tests/TestsCookies.hs ./gif.hs ./tests/TestsGif.hs
