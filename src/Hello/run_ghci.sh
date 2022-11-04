e900282b8714aa6cd5e24ae930f61fe171e39551
acerion <acerion@wp.pl>
2022-11-04 21:26:47 +0100

    Adding missing files to ghci's command line

--- src/Hello/run_ghci.sh
+++ src/Hello/run_ghci.sh
@@ -2,9 +2,12 @@ ghci -fobject-code \
      -XOverloadedStrings \
      ./*.hs \
      ./Css/*.hs \
+     ./Css/Parser/*.hs \
+     ./Dw/*.hs \
      ./Html/*.hs \
      ./Tests/*.hs \
      ./Tests/Css/*.hs \
+     ./Tests/Css/Parser/*.hs \
      ./Tests/Html/*.hs \
      ./Tests/TestUtils/*.hs \
      ./Ffi/Css/SelectorLink.hs
