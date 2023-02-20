544dbce39477407b1337579d80d5d8903dfd053e
acerion <acerion@wp.pl>
2023-02-20 20:38:59 +0100

    CSS: Removing unit tests of cssGetMinSpecState
    The cssGetMinSpecState is not needed in cascading code that doesn't do match
    caching. The function will be removed in next commit, so unit tests for the
    function are not needed.

--- src/Hello/Tests/Css/Cascade.hs
+++ src/Hello/Tests/Css/Cascade.hs
@@ -21,23 +21,6 @@ where
 
 -- import Debug.Trace
 import Test.HUnit
-import qualified Data.Text as T
-
-import Hello.Css.Cascade
-import Hello.Tests.Css.CascadeTestData
-
-
-
-
-cssGetMinSpecStateTestFunction :: [GetMinSpecStateTestData] -> T.Text
-cssGetMinSpecStateTestFunction []     = ""
-cssGetMinSpecStateTestFunction (x:xs) = if output /= expectedOutput
-                                        then T.pack ("output = " ++ show output ++ ", expectedOutput  = " ++ show expectedOutput)
-                                        else cssGetMinSpecStateTestFunction xs
-  where
-    input          = matchingRules x
-    output         = cssGetMinSpecState input
-    expectedOutput = specificityState x
 
 
 
@@ -45,9 +28,6 @@ cssGetMinSpecStateTestFunction (x:xs) = if output /= expectedOutput
 testCases :: [Test]
 testCases =
   [
-    -- If some error is found, test function returns some data (e.g. non-empty
-    -- string or test index) which can help identify which test failed.
-    TestCase (do assertEqual "manual tests of cssGetMinSpecState"                     "" (cssGetMinSpecStateTestFunction cssGetMinSpecStateTestData))
   ]
 
 
