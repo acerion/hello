acerion/hello

commit 6869d6e8bfc9586a3d93b5f9a00f5637ea524489

CSS: Fixing source of a minor issue
The issue resulted in some parts of test page not display correctly.

src/styleengine.cc
@@ -501,8 +501,8 @@ Style * StyleEngine::makeStyle(int styleNodeIndex, BrowserWindow *bw)
 
 Style * StyleEngine::makeWordStyle(BrowserWindow *bw) {
    StyleAttrs attrs = *getStyle (bw);
-   // TODO: original code called this function. Retore it?
-   //attrs.resetNonInheritedValues ();
+   attrs.c_style_attrs_ref = ffiStyleAttrsCtor();
+   attrs.resetNonInheritedValues();
 
    StyleNode * node = getCurrentNode(this);
    if (node->inheritBackgroundColor) {

commit 7cb84a789e02306b7e9bd5732d98c509ab5dc977

Fixing problem with non-clickable links.
The problem was introduced in 6869d6e8bfc9586a3d93b5f9a00f5637ea524489.

src/Hello/Dw/StyleAttrsGlobal.hs
@@ -35,6 +35,7 @@ Store global variables with StyleAttrs objects.
 module Hello.Dw.StyleAttrsGlobal
   (
     globalStyleAttrsCtor
+  , globalStyleAttrsCopyCtor
   , globalStyleAttrsUpdate
   , globalStyleAttrsPut
   , globalStyleAttrsGet
@@ -68,6 +69,14 @@ myGlobalStyleAttrs = unsafePerformIO (newIORef [])
 -- Second argument is a constructor of default element.
 globalStyleAttrsCtor :: IO Int
 globalStyleAttrsCtor = globalContainerCtor myGlobalStyleAttrs defaultStyleAttrs
+
+
+
+
+-- A copy constructor.
+-- Construct a new item from given item.
+globalStyleAttrsCopyCtor :: Int -> IO Int
+globalStyleAttrsCopyCtor = globalContainerCopyCtor myGlobalStyleAttrs
 
 
 

src/Hello/Ffi/Dw/StyleAttrsGlobal.hsc
@@ -58,6 +58,7 @@ import Hello.Ffi.Utils
 
 
 foreign export ccall "ffiStyleAttrsCtor" ffiStyleAttrsCtor :: IO CInt
+foreign export ccall "ffiStyleAttrsCopyCtor" ffiStyleAttrsCopyCtor :: CInt -> IO CInt
 
 
 foreign export ccall "ffiStyleAttrsInitValues" ffiStyleAttrsInitValues :: CInt -> IO ()
@@ -173,6 +174,15 @@ ffiStyleAttrsCtor :: IO CInt
 ffiStyleAttrsCtor = do
   ref :: Int <- fmap fromIntegral globalStyleAttrsCtor
   return . fromIntegral $ ref
+
+
+
+
+-- A copy constructor.
+-- Construct a new style attribute from given style attribute.
+ffiStyleAttrsCopyCtor :: CInt -> IO CInt
+ffiStyleAttrsCopyCtor cExistingRef = do
+  fmap fromIntegral $ globalStyleAttrsCopyCtor . fromIntegral $ cExistingRef
 
 
 

src/Hello/GlobalContainer.hs
@@ -34,6 +34,7 @@ module Hello.GlobalContainer
   (
     globalContainerPut
   , globalContainerCtor
+  , globalContainerCopyCtor
   , globalContainerGet
   , globalContainerUpdate
   )
@@ -71,6 +72,19 @@ globalContainerCtor globalContainer defaultMaker = do
   let new = old ++ [ defaultMaker ]
   writeIORef globalContainer new
   return (length new - 1)
+
+
+
+
+-- Copy given item, add it to container as new item. Return reference to new
+-- item.
+globalContainerCopyCtor :: IORef [a] -> Int -> IO Int
+globalContainerCopyCtor globalContainer itemRef = do
+  container <- readIORef globalContainer
+  let item = container !! itemRef
+      container' = container ++ [ item ] -- New item is being added as copy of existing item.
+  writeIORef globalContainer container'
+  return (length container' - 1)
 
 
 

src/Hello/hello.h
@@ -147,6 +147,7 @@ typedef struct c_style_attrs_t {
 
 
 int ffiStyleAttrsCtor(void);
+int ffiStyleAttrsCopyCtor(int ref);
 void ffiStyleAttrsInitValues(int ref);
 bool ffiStyleAttrsEqual(int ref1, int ref2);
 int ffiStyleAttrsHashValue(int ref);

src/styleengine.cc
@@ -469,6 +469,8 @@ Style * StyleEngine::makeStyle(int styleNodeIndex, BrowserWindow *bw)
    // however overwritten in style_attrs_make_ui_objects().
    StyleAttrs styleAttrs = parentStyleAttrs;
    styleAttrs.c_style_attrs_ref = ffiStyleAttrsCtor();
+   /* TODO: check if we could use ffiStyleAttrsCopyCtor() above and remove
+      the call to ffiStyleAttrsCopy(). */
    ffiStyleAttrsCopy(styleAttrs.c_style_attrs_ref, parentStyleAttrs.c_style_attrs_ref);
 
    // Ensure that StyleEngine::style0() has not been called before for
@@ -513,7 +515,7 @@ Style * StyleEngine::makeStyle(int styleNodeIndex, BrowserWindow *bw)
 
 Style * StyleEngine::makeWordStyle(BrowserWindow *bw) {
    StyleAttrs attrs = *getStyle (bw);
-   attrs.c_style_attrs_ref = ffiStyleAttrsCtor();
+   attrs.c_style_attrs_ref = ffiStyleAttrsCopyCtor(attrs.c_style_attrs_ref);
    attrs.resetNonInheritedValues();
 
    StyleNode * node = getCurrentNode(this);